7685e0b23a468862113849ba1a86a31dee39a0a2
sunilsuraci <suracisunil@gmail.com>
2021-11-16 17:39:51 +0100

    commit finito

---
+++ src/org/generation/italy/CalcolaBiglietto.java
@@ -0,0 +1,41 @@
+package org.generation.italy;
+
+import java.util.Scanner;
+
+public class CalcolaBiglietto { // £ al km=0.21, 20% in meno a >18, 40% in - a <65// 
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		//informazzioni
+		double prezzoDelBiglietto = 0.21;
+		double kmDafare;
+		int età;
+		double sconto18 = 0.2;
+		double sconto65 = 0.4;
+		
+		
+		//calcolo km/sconto
+		
+		System.out.print("Inserire i km da percorrere: ");
+		kmDafare = scanner.nextDouble();
+		System.out.print("Digita la tua età: ");
+		età = scanner.nextInt();
+		double costoBigliettoAlKm = prezzoDelBiglietto * kmDafare;
+		//double prezzoTotalebiglitto = costoBigliettoAlKm *
+		
+		if(età < 18) {
+		   System.out.println("Cost del biglietto: " + (costoBigliettoAlKm - (costoBigliettoAlKm * sconto18))+ "euro");
+		} else if(età > 17) {
+			System.out.println("Cost del biglietto: " + (costoBigliettoAlKm - (costoBigliettoAlKm * sconto65))+ "euro");
+		}
+		
+		
+		
+		
+		scanner.close();
+	}
+
+}
